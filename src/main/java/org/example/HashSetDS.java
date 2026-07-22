7a9325a3c028b600e8b3131c56dc20928536b417
Sahid Reza <sahidreza976@gmail.com>
2026-07-22 08:49:27 +0530

    Add insertion

--- src/main/java/org/example/HashSetDS.java
+++ src/main/java/org/example/HashSetDS.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class HashSetDS {
     public static void main(String[] args) {
 
+        HashSet<Integer>set = new HashSet<>();
+
+        // insertion
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        System.out.println(set);
     }
 }
